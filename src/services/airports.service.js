90e78b0d676bad491a458058224f7144b0109c97
Student1-35182496 <Student1-35182496@LODSPRODMCA.onmicrosoft.com>
2023-11-03 13:57:16 -0700

    fixes AB#190

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -7,6 +7,7 @@ class AirportsService {
         this._airports = airports;
     }
 
+    //sort 2
     getAll(){
         return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
     }
